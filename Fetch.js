4013066b6ce0bdfce36e99fa4e74dc8a178264cc
Tamimislam47 <tamim.furious@gmail.com>
2024-12-07 06:01:50 -0500

    select consultationList and filter doctorsList, make Doctor and consutlation json files

--- Fetch.js
+++
@@ -1,9 +0,0 @@
-async function Fetch(link) {
-
-  const response = await fetch(link);
-  const data = await response.json();
-
-  return data;
-}
-
-export { Fetch };
--- consultation/consultation.js
+++ consultation/consultation.js
@@ -37,31 +37,49 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 });
 
-
 async function getData() {
   try {
     const response = await fetch("../consultation/consultation.json");
+
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
     const data = await response.json();
+
     const gridDiv = document.getElementById("gridDiv");
 
     let cards = "";
 
-    data.forEach((item) => {
-      const { img, title, text } = item;
+    // Generate cards HTML
+    data.forEach((item, index) => {
+      const { img, department, text } = item;
 
       cards += `
-        <div class="card">
+        <div id="card-${index}" class="card">
           <div class="cardImgDiv">
-            <img src="${img}" alt="${title} image" />
+            <img src="${img}" alt="${department} image" />
           </div>
           <div class="cardTextSide">
-            <h2>${title}</h2>
+            <h2>${department}</h2>
             <p>${text}</p>
           </div>
         </div>`;
     });
 
     gridDiv.innerHTML = cards;
+
+    // Add click events
+    data.forEach((item, index) => {
+      const cardId = `card-${index}`;
+      const cardElement = document.getElementById(cardId);
+
+      cardElement.addEventListener("click", () => {
+        const queryParams = new URLSearchParams({
+          departmentId: item.id,
+        }).toString();
+
+        window.location.href = `specialityList.html?${queryParams}`;
+      });
+    });
   } catch (error) {
     console.error("Error fetching or displaying data:", error);
   }
