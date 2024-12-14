b32b325f39010611dd51ab8d200465d6cd77db29
Sumaiya Liza <lizasumaiya81@gmail.com>
2024-12-15 04:30:47 +0600

    style

--- specialityList.js
+++ specialityList.js
@@ -1,44 +1,6 @@
-import { Fetch } from "../FetchData.js";
-
-async function loadHTML(filePath, targetElementId) {
-  try {
-    const response = await fetch(filePath);
-    if (!response.ok)
-      throw new Error(`Failed to load ${filePath}: ${response.statusText}`);
-    const htmlContent = await response.text();
-    document.getElementById(targetElementId).innerHTML = htmlContent;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", async function () {
-  try {
-    await Promise.all([loadHTML("../nav/nav.html", "header")]);
-    console.log("All HTML files loaded successfully.");
-
-    const loginButton = document.querySelector("#loginBtn");
-    if (loginButton) {
-      loginButton.addEventListener("click", () => {
-        window.location.href = "../login/login.html";
-      });
-    }
+import { Fetch } from "./FetchData.js";
 
     // Assuming this script is in the main file where you want to load the footer
-    fetch("../Footer/footer.html")
-      .then((response) => {
-        if (!response.ok) throw new Error("Failed to fetch footer");
-        return response.text();
-      })
-      .then((data) => {
-        document.querySelector(".footer").innerHTML = data;
-      })
-      .catch((error) => console.error("Error loading footer:", error));
-  } catch (error) {
-    console.error("Error loading HTML files:", error);
-  }
-});
-
 function clearCards() {
   const cardContainer = document.querySelector(".cardContainer");
   if (cardContainer) {
@@ -272,3 +234,5 @@ function renderSpecialties(specialties) {
 selectDoctorFilter();
 renderRowCards();
 checkboxFilter();
+
+
